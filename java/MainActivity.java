e9a1fd0d34e92338769172a54788855770f7f66a
rfgomba <161476620+rfgomba@users.noreply.github.com>
2026-09-20 16:51:16 +0200

    Implement bottom navigation in MainActivity

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,1 +1,44 @@
+package com.smartpantry.manager;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_pantry);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_pantry) {
+                    // Current Home View (Pantry List)
+                    return true;
+                } else if (itemId == R.id.nav_suggestions) {
+                    Intent intent = new Intent(MainActivity.this, SuggestedRecipesActivity.class);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_settings) {
+                    Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
